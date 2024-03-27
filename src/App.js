c0d02f4b9e85fc16a180b04fbacf47badf48d20b
clairep9 <clairepetitfrere@pursuit.org>
2024-03-27 19:00:55 -0400

    pushed claire's

--- src/App.js
+++ src/App.js
@@ -5,9 +5,18 @@ import HousingProject from './HousingDetials';
 
 function App() {
   return (
-    <div className="App">
-      <HousingProject />
+    <div className="container">
+      <header className="homepage__header">
+        <h1>Header</h1>
+      </header>
+      <div className="homepage__body">
+      <h1 className='homepage__title'>Affordable Homes</h1> 
+      <input type="text" className="search__bar" placeholder="Enter a neighborhood or ZIP code" />
+      </div>
     </div>
+
+
+
   );
 }
 
