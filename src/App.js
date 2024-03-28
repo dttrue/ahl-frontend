351b4a04e0b370e3ca03d5ecb55030c90327628e
lawsonjerry <williambookman@pursuit.org>
2024-03-27 22:33:47 -0400

    added signup function with firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import SignIn from './components/auth/Signin';
+import SignUp from './components/auth/SignUp';
 
 function App() {
   return (
@@ -13,10 +14,12 @@ function App() {
       <h1 className='homepage__title'>Affordable Homes</h1> 
       <input type="text" className="search__bar" placeholder="Enter a neighborhood or ZIP code" />
       <Link to="/signin">Sign In</Link>
+      <Link to="/signup">Create Account</Link>
 
       <Routes>
           <Route path="/" element={<div>Home Page Content</div>} />  {/* Replace with your homepage content */}
           <Route path="/signin" element={<SignIn />} />  {/* SignIn component for login */}
+          <Route path="/signup" element={<SignUp />} />  {/* SignIn component for login */}
         </Routes>
 
       </div>
